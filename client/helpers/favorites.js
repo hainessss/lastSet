93c35764216732ea8ba59fb2b5c25102e4cbe483
alex haines <hainesaxiv@gmail.com>
2014-09-22 13:34:27 -0700

    added heroku

--- client/helpers/favorites.js
+++ client/helpers/favorites.js
@@ -1,4 +1,4 @@
-//local collection that holds all tracks obtained from rdio and soundcloud api
+//local collection that holds all tracks obtained from rdio and soundcloud apmeei
 Sounds = new Meteor.Collection(null);
 
 

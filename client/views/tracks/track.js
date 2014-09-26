2f4f5bbff562f586b004cb45c0a102c582646b50
alex haines <hainesaxiv@gmail.com>
2014-09-26 14:29:04 -0700

    fixed playing class bug

--- client/views/tracks/track.js
+++ client/views/tracks/track.js
@@ -23,10 +23,20 @@ Template.track.events({
 
   'click .remove': function(e) {
     var track = Tracks.findOne(e.currentTarget.dataset.trackid);
+    var element = $('#playlist').find('.track-link[data-trackid=' + e.currentTarget.dataset.trackid + ']')[0];
+    var removedTrackIndex = $(e.currentTarget).parents('#playlist').find('.track-link').index(element);
+    var currentTrackIndex = Session.get('currentTrack');
 
     Meteor.call('deletePlaylistTrack', track, function(error, result) {
       if (error) {
         throwError(error.reason);
+      } else {
+        if (Session.get('playing')) {
+          if (currentTrackIndex > removedTrackIndex) {
+            $('#playlist').find('.track:eq(' + Session.get('currentTrack') + ')').removeClass('playingTrack');
+            Session.set('currentTrack', currentTrackIndex - 1);
+          }
+        }
       }
     });
   }

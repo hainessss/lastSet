490012ae74f6485317e9286ea52d5bb9e7283b98
alex haines <hainesaxiv@gmail.com>
2014-09-25 22:39:08 -0700

    added remove track feature

--- client/views/tracks/track.js
+++ client/views/tracks/track.js
@@ -11,12 +11,20 @@ Template.track.helpers({
 
 Template.track.events({
   'click .playlist-dropdown': function(e) {
-    e.preventDefault();
-
     var track = Tracks.findOne({soundId: e.currentTarget.dataset.soundid});
 
     Meteor.call('addPlaylistTrack',
       this._id, track, function(error, result) {
+      if (error) {
+        throwError(error.reason);
+      }
+    });
+  },
+
+  'click .remove': function(e) {
+    var track = Tracks.findOne(e.currentTarget.dataset.trackId);
+
+    Meteor.call('deletePlaylistTrack', track function(error, result) {
       if (error) {
         throwError(error.reason);
       }
--- collections/tracks.js
+++ collections/tracks.js
@@ -5,7 +5,7 @@ Meteor.methods({
   addPlaylistTrack: function(playlistId, trackData) {
     var user = Meteor.user();
     var sound = trackData;
-    var trackAlreadyExists = Tracks.find({})
+    var trackAlreadyExists = Tracks.findOne({soundId: sound.soundId, pid: playlistId});
 
     if(!user) {
       throw new Meteor.Error(401, "You must be logged in to create a new playlist");
@@ -15,13 +15,28 @@ Meteor.methods({
       throw new Meteor.Error(422, "Your must have a valid track to add");
     }
 
+    if(trackAlreadyExists) {
+      throw new Meteor.Error(422, "This track already exits in this playlist");
+    }
+
     var track = _.extend(_.pick(sound, 'soundId', 'track_url', 'artist', 'name', 'artwork_url', 'duration'),
-      {pid: playlistId, submitted: new Date().getTime(), nowPlaying: null, type: 'track'
+      {pid: playlistId, submitted: new Date().getTime(), nowPlaying: null, contributer: user._id, type: 'track'
     });
 
     Tracks.insert(track);
 
     return playlistId;
+  },
+
+  deletePlaylistTrack: function(track) {
+    var playlist = Playlists.findOne(track.pid);
+    var user = Meteor.user();
+
+    if (playlist.userId !== user._id || track.contributer !== user._id) {
+      throw new Meteor.Error(422, "You must be the playlist admin or have contributed the song to remove it");
+    }
+
+    return Tracks.remove({_id: track._id});
   }
 });
 
